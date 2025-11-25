c261daa841700f1be0d671aa9f2817a5467897be
mohamed-engineer <plcacademy123@gmail.com>
2025-11-25 22:59:04 +0200

    first commit - Next.js educational platform

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -105,8 +105,8 @@ export default function Hero() {
         <Image
           src="/1.png" // حط اللابتوب بتاعك هنا
           alt="Laptop"
-          width={750}
-          height={500}
+          width={100}
+          height={100}
           className="drop-shadow-2xl"
         />
       </motion.div>
